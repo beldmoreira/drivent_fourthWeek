a5d4749ba596679edeabd90de7750ff22f186802
Isabela <isabela_cmoreira@hotmail.com>
2022-12-05 17:50:50 -0300

    feat:final version of booking repository

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -10,19 +10,52 @@ async function getBooking(userId: number) {
   });
 }
 
-async function postBooking(booking: CreateBookingParams) {
+async function postBooking(userId: number, roomId: number) {
   return prisma.booking.create({
     data: {
-      ...booking,
+      userId: userId,
+      roomId: roomId,
     },
   });
 }
 
-export type CreateBookingParams = Omit<Booking, "id" | "createdAt" | "updatedAt">;
+async function findRooms(roomId: number) {
+  return prisma.room.findFirst({
+    where: { id: roomId },
+    include: {
+      Booking: true,
+    },
+  });
+}
+
+async function updateBooking(bookingId: string, roomId: number) {
+  return prisma.booking.update({
+    where: {
+      id: Number(bookingId),
+    },
+    data: {
+      roomId: roomId,
+    },
+  });
+}
+
+async function findBookingById(bookingId: string) {
+  return await prisma.booking.findFirst({
+    where: {
+      id: Number(bookingId),
+    },
+    include: {
+      Room: true,
+    },
+  });
+}
 
 const bookingRepository = {
   getBooking,
   postBooking,
+  findRooms,
+  updateBooking,
+  findBookingById,
 };
 
 export default bookingRepository;
